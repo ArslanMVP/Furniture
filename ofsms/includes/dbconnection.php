1c734ac92ac9d5e38314df51a0ff3ae7db9cc6bc
ArslanMVP <120491462+ArslanMVP@users.noreply.github.com>
2024-04-01 16:24:15 +0500

    Update dbconnection.php

--- ofsms/includes/dbconnection.php
+++ ofsms/includes/dbconnection.php
@@ -1,16 +1,16 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','ofsmsdb');
+define('DB_HOST','ofsmsdb.cwnfuiidldzb.us-east-1.rds.amazonaws.com');
+define('DB_USER','admin'); // Имя пользователя
+define('DB_PASS','Test1234'); // Пароль
+define('DB_NAME','ofsmsdb'); // Имя базы данных
 // Establish database connection.
 try
 {
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 }
 catch (PDOException $e)
 {
-exit("Error: " . $e->getMessage());
+    exit("Error: " . $e->getMessage());
 }
 ?>
